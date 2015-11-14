07b85b7e6ec97313b9fec177054bd4ddf5e45643
Roger <rlau4545@gmail.com>
2015-11-13 21:09:04 -0800

    navigation has been learned

--- app/src/main/java/com/csm117/ridesplanner/NavigationActivity.java
+++ app/src/main/java/com/csm117/ridesplanner/NavigationActivity.java
@@ -1,5 +1,6 @@
 package com.csm117.ridesplanner;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -84,6 +85,8 @@ public class NavigationActivity extends AppCompatActivity
 
         if (id == R.id.nav_camara) {
             // Handle the camera action
+            Intent nextScreen = new Intent(getApplicationContext(),  ViewRides.class);
+            startActivity(nextScreen);
         } else if (id == R.id.nav_gallery) {
 
         } else if (id == R.id.nav_slideshow) {
--- app/src/main/java/com/csm117/ridesplanner/ViewPersonsList.java
+++ app/src/main/java/com/csm117/ridesplanner/ViewPersonsList.java
@@ -1,10 +1,16 @@
 package com.csm117.ridesplanner;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
@@ -17,12 +23,12 @@ import com.csm117.ridesplanner.persons.Person;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ViewPersonsList extends AppCompatActivity {
+public class ViewPersonsList extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_view_persons_list);
+        setContentView(R.layout.activity_view_persons_list_navigation);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -48,16 +54,36 @@ public class ViewPersonsList extends AppCompatActivity {
         ArrayAdapter<Person> adapter = new PersonsListAdapter(this, persons);
         listview.setAdapter(adapter);
 
-        //gridview.setOnItemClickListener(new AdapterView.OnItemClickListener
-        //// () {//
-        //    public void onItemClick(AdapterView<?> parent, View v,//
-        //                            int position, long id) {//
-        //        Toast.makeText(getApplicationContext(),//
-        //                ((TextView) v).getText(), Toast.LENGTH_SHORT).show(// );
-        //    }//
-        //});
 
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
 
     }
 
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camara) {
+            // Handle the camera action
+            Intent nextScreen = new Intent(getApplicationContext(),  ViewRides.class);
+            startActivity(nextScreen);
+        } else if (id == R.id.nav_gallery) {
+            Intent nextScreen = new Intent(getApplicationContext(),  ViewPersonsList.class);
+            startActivity(nextScreen);
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
 }

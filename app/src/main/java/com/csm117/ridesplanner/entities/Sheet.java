38fc06c0a259a47fa8fe10cd3976f055b156a8fd
Roger <rlau4545@gmail.com>
2015-11-19 21:28:43 -0800

    julian try this

--- app/src/main/java/com/csm117/ridesplanner/entities/Sheet.java
+++ app/src/main/java/com/csm117/ridesplanner/entities/Sheet.java
@@ -23,28 +23,47 @@ public class Sheet {
     private static List<RideGroup> rideGroups_ = new ArrayList<RideGroup>();
     private static List<Person> unsentPersons_ = new ArrayList<Person>();
 
-    private Sheet(){
+    private Sheet() {
 
     }
 
-    public static List<RideGroup> getRideGroups(){
+    public static List<RideGroup> getRideGroups() {
         return rideGroups_;
     }
-    public static List<Person> getUnsentPersons(){
+
+    public static List<Person> getUnsentPersons() {
         return unsentPersons_;
     }
 
-    public static void getDataFromOnlineSheet(){
-        class MyCallback implements OnTaskCompleted{
-            public void onTaskCompleted(Object output){
+    public static void getDataFromOnlineSheet() {
+        class MyCallback implements OnTaskCompleted {
+            public void onTaskCompleted(Object output) {
                 //do your stuff with the result stuff
                 Log.d("credential", "CREDENTIALS WORKED!");
-                for (ArrayList<String> car : (ArrayList<ArrayList<String>>)
-                        output) {
-                    for(String s : car) {
-                        Log.d("tag", s);
+                rideGroups_.clear();
+                unsentPersons_.clear();
+
+
+                for (ArrayList<String> car : (ArrayList<ArrayList<String>>) output) {
+                    List<Person> riders = new ArrayList<Person>();
+                    Person driver = null;
+                    for (int i = 0; i < car.size(); i++) {
+                        if (i == 0)
+                            driver = new Driver(car.get(i));
+                        else
+                            riders.add(new Rider(car.get(i)));
                     }
+                    rideGroups_.add(new RideGroup(driver, riders));
                 }
+
+                //TODO: make an unsent persons format
+                for (int k = 0; k < 5; k++) {
+                    unsentPersons_.add(new Driver("driver" + k));
+                }
+                for (int k = 0; k < 20; k++) {
+                    unsentPersons_.add(new Rider("rider" + k));
+                }
+                sortNames();
             }
         }
 
@@ -52,8 +71,6 @@ public class Sheet {
         objectList.add("1p8IvZm5UWtO6wY8LO8nmhYoUImyc1wgqilGr9ictZkc"); // sheet id
         MyCallback cb = new MyCallback();
         new MakeRequestTask("getData", objectList, cb).execute();
-        //get
-        // mCredential
     }
 
     public static void pushDataToOnlineSheet(){

6f6e68082ce929c65ec29f68af7fb741f50fc478
Roger <rlau4545@gmail.com>
2015-11-21 23:45:58 -0800

    started sendcar functionality

---
+++ app/src/main/java/com/csm117/ridesplanner/onClickListeners/SendCarFabListener.java
@@ -0,0 +1,15 @@
+package com.csm117.ridesplanner.onClickListeners;
+
+import android.view.View;
+
+/**
+ * Created by Roger on 11/21/2015.
+ */
+//if there is exactly one driver selected
+    //then the sendcar button appears
+    //sending a car grays out everyone in the car
+public class SendCarFabListener implements View.OnClickListener{
+    public void onClick(View view) {
+
+    }
+}
